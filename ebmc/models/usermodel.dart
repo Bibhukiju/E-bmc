0ddabed824d7423a9d9a0e79cbb9eb224ac43346
bibhukiju <bibhukiju@gmail.com>
2020-07-06 12:57:50 +0545

    user model is created

--- ebmc/models/usermodel.dart
+++ ebmc/models/usermodel.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 Createuser createuserFromJson(String str) =>
     Createuser.fromJson(json.decode(str));
+
 String createuserToJson(Createuser data) => json.encode(data.toJson());
 
 class Createuser {
@@ -9,25 +10,21 @@ class Createuser {
     this.name,
     this.email,
     this.password,
-    this.secondPassword,
   });
 
   String name;
   String email;
   String password;
-  String secondPassword;
 
   factory Createuser.fromJson(Map<String, dynamic> json) => Createuser(
         name: json["name"],
         email: json["email"],
         password: json["password"],
-        secondPassword: json["second-password"],
       );
 
   Map<String, dynamic> toJson() => {
         "name": name,
         "email": email,
         "password": password,
-        "second-password": secondPassword,
       };
 }
