4eaf5d10061095c38fc12e86a4dafddc044a1cce
bibhukiju <bibhukiju@gmail.com>
2020-07-06 10:59:28 +0545

    api is tested still not working

--- ebmc/lib/screens/signup.dart
+++ ebmc/lib/screens/signup.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import './Loginpage.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -11,6 +14,33 @@ class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController repass = TextEditingController();
   final TextEditingController email = TextEditingController();
   final TextEditingController otp = TextEditingController();
+  bool _isLoading = false;
+
+  signUp(String username, String email, String firstPassword,
+      String secondPassword) async {
+    Map data = {
+      // 'name': username,
+      'email': email,
+      'first-password': firstPassword,
+      'second-password': secondPassword,
+    };
+    print('\n' + email + '\n ' + firstPassword + '\n' + secondPassword);
+    var jsonResponse;
+    var response =
+        await http.post('https://ebmc.herokuapp.com/register', body: data);
+    if (response.statusCode == 200) {
+      jsonResponse = jsonDecode(response.body);
+      if (jsonResponse != null) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    } else {
+      print(response.body);
+      print('hello');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,54 +110,60 @@ class _SignUpPageState extends State<SignUpPage> {
                     color: Colors.blue,
                     child: InkWell(
                       onTap: () {
-                        showDialog(
-                            context: context,
-                            child: SimpleDialog(
-                              contentPadding: EdgeInsets.all(20),
-                              titlePadding: EdgeInsets.all(20),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)),
-                              title: Text("Enter your OTP"),
-                              children: <Widget>[
-                                TextField(
-                                  controller: otp,
-                                  maxLength: 6,
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      labelText: "Your OTP",
-                                      labelStyle: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.grey,
-                                      )),
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: <Widget>[
-                                    SimpleDialogOption(
-                                      child: Text(
-                                        "Submit",
-                                        style:
-                                            TextStyle(color: Colors.lightBlue),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                        Navigator.of(context)
-                                            .pushNamed('loginpage');
-                                      },
-                                    ),
-                                    SimpleDialogOption(
-                                      child: Text(
-                                        "Resend",
-                                        style:
-                                            TextStyle(color: Colors.lightBlue),
-                                      ),
-                                      onPressed: () {},
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ));
+                        // showDialog(
+                        //     context: context,
+                        //     child: SimpleDialog(
+                        //       contentPadding: EdgeInsets.all(20),
+                        //       titlePadding: EdgeInsets.all(20),
+                        //       shape: RoundedRectangleBorder(
+                        //           borderRadius: BorderRadius.circular(10)),
+                        //       title: Text("Enter your OTP"),
+                        //       children: <Widget>[
+                        //         TextField(
+                        //           controller: otp,
+                        //           maxLength: 6,
+                        //           decoration: InputDecoration(
+                        //               border: InputBorder.none,
+                        //               labelText: "Your OTP",
+                        //               labelStyle: TextStyle(
+                        //                 fontWeight: FontWeight.bold,
+                        //                 color: Colors.grey,
+                        //               )),
+                        //         ),
+                        //         Row(
+                        //           mainAxisAlignment:
+                        //               MainAxisAlignment.spaceEvenly,
+                        //           children: <Widget>[
+                        //             SimpleDialogOption(
+                        //               child: Text(
+                        //                 "Submit",
+                        //                 style:
+                        //                     TextStyle(color: Colors.lightBlue),
+                        //               ),
+                        //               onPressed: () {
+                        //                 Navigator.pop(context);
+                        //                 Navigator.of(context)
+                        //                     .pushNamed('homepage');
+                        //               },
+                        //             ),
+                        //             SimpleDialogOption(
+                        //               child: Text(
+                        //                 "Resend",
+                        //                 style:
+                        //                     TextStyle(color: Colors.lightBlue),
+                        //               ),
+                        //               onPressed: () {},
+                        //             ),
+                        //           ],
+                        //         )
+                        //       ],
+                        //     ));
+                        signUp(username.text, email.text, password.text,
+                            repass.text);
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                                builder: (BuildContext context) => HomePage()),
+                            (Route<dynamic> route) => false);
                       },
                       child: Container(
                         padding: EdgeInsets.all(10),
